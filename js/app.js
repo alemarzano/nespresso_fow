291c6be6c2c99cdae24d04fc9320f16cd1045352
Ale Marzano <alejandromarzano@encendercomunicacion.com>
2020-08-20 18:29:32 -0300

    Textos OK

---
+++ js/app.js
@@ -0,0 +1,70 @@
+"use strict";
+
+var app = angular.module('FowApp', []);
+app.directive('myAmount', function () {
+  return {
+    restrict: 'A',
+    link: function link(scope, elem, attrs) {
+      scope.$watch(attrs['ngModel'], function (v) {
+        console.log('value changed, new value is: ' + v);
+      });
+    }
+  };
+});
+app.controller('oradoresCtrlr', function ($scope) {
+  $scope.oradores = [{
+    "id": "01",
+    "nombre": "Alejandro Melamed",
+    "foto": "melamed.jpg",
+    "rol": "Ambassador",
+    "descripcion": "Director de Humanize Consulting. Referente en temas del futuro del trabajo, liderazgo y cultura organizacional. Fue VP  de RRHH de Coca Cola, Gerente de RRHH de Molinos Rio de la Plata y consultor senior de Arthur Andersen, entre otras actividades."
+  }, {
+    "id": "02",
+    "nombre": "Facundo Manes",
+    "foto": "manes.jpg",
+    "rol": "Speaker",
+    "descripcion": "Neurocientífico y neurólogo. Investigador del CONICET y presidente honorario de Fundación INECO. Profesor de neurociencias cognitivas, presidente electo de la ISFTD y consultor del MRC Cognition and Brain Sciences Unit de la Universidad de Cambridge."
+  }, {
+    "id": "03",
+    "nombre": "Carla Quiroga",
+    "foto": "quiroga.jpg",
+    "rol": "Moderadora",
+    "descripcion": "Periodista con más de 25 años de experiencia  en los medios. Trabajó en canal 7, 26 TV, Telefe y P&E. También en la revista Negocios, el diario El Cronista y la revista Apertura. Actualmente es editora de los suplementos de propiedades e inmuebles comerciales del diario La Nación y columnista en la LN+."
+  }];
+  $scope.oradorActivo = $scope.oradores[0];
+  $('.owl-oradores').on('translated.owl.carousel', function (e) {
+    $scope.oradorActivo = [];
+    $('.orador-text').css({
+      opacity: 0
+    });
+    setTimeout(function () {
+     var idActivo = $('.owl-oradores .owl-item')[e.item.index];
+      idActivo = $(idActivo).children('.item').data('index');
+
+      for (var orador in $scope.oradores) {
+        var $this = $scope.oradores[orador];
+
+        if ($this.id == idActivo) {
+          $('.orador-text').css({
+            opacity: 1
+          });
+          $scope.oradorActivo = $this;
+          $scope.$apply();
+        }
+      }
+    }, 250);
+  });
+});
+app.controller('citasCtrlr', function ($scope) {
+  $scope.citas = [{
+    "quote": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim quae cumque asperiores facere dolorem.Cumque?",
+    "foto": "foto.webp",
+    "nombre": "Lorem, ipsum",
+    "puesto": "Lorem ipsum dolor, sit amet consectetur adipisicing elit."
+  }, {
+    "quote": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim quae cumque asperiores facere dolorem.Cumque?",
+    "foto": "foto.webp",
+    "nombre": "Lorem, ipsum",
+    "puesto": "Lorem ipsum dolor, sit amet consectetur adipisicing elit."
+  }];
+});
---
+++ js/events.js
@@ -0,0 +1,222 @@
+"use strict";
+
+var icon = $('#click');
+var overlay = $("#overlay");
+var menuLinks = $('#menulinks');
+
+function openNav() {
+  // document.getElementById("menulinks").style.width = "250px";
+  // document.querySelector('body').style.overflow = "hidden"
+  // overlay.style.opacity = '.7'
+  // overlay.style.height = '100vh'
+  // icon.setAttribute('onclick', 'closeNav()');
+  // icon.innerHTML = "<img src='images/close.svg' alt=''>";
+  $(menuLinks).css({
+    'width': '250px',
+    'top': '52px'
+  });
+  $(overlay).css({
+    'opacity': '.7',
+    'display': 'block'
+  });
+  $(icon).attr('onclick', 'closeNav()');
+  $(icon).html("<img src='images/close.svg' alt=''>");
+}
+
+function closeNav() {
+  // document.getElementById("menulinks").style.width = "0";
+  // document.querySelector('body').style.overflow = "inherit"
+  // overlay.style.opacity = '0';
+  // overlay.style.height = '0'
+  // icon.setAttribute('onclick', 'openNav()');
+  // icon.innerHTML = "<img src='images/menu.svg' alt=''>";
+  $(menuLinks).css({
+    'width': '0'
+  });
+  $(overlay).css({
+    'display': 'none',
+    'opacity': '0'
+  });
+  $(icon).attr('onclick', 'openNav()');
+  $(icon).html("<img src='images/menu.svg' alt=''>");
+}
+
+$(overlay).click(function () {
+  closeNav();
+});
+var tabcontent = document.getElementsByClassName("tabcontent");
+tabcontent[0].style.display = "block";
+
+function openCity(evt, cityName) {
+  // Declare all variables
+  var i, tablinks; // Get all elements with class="tabcontent" and hide them
+
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  } // Get all elements with class="tablinks" and remove the class "active"
+
+
+  tablinks = document.getElementsByClassName("tablinks");
+
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  } // Show the current tab, and add an "active" class to the button that opened the tab
+
+
+  document.getElementById(cityName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
+
+$(document).ready(function () {
+  /**********/
+  var owlInv = $('#owl-inv');
+  owlInv.owlCarousel({
+    loop: true,
+    nav: true,
+    navText: ["<img src='./images/left.svg'>", "<img src='./images/right.svg'>"],
+    dots: false,
+    items: 1,
+    autoplay: true,
+    autoplayTimeout: 2000,
+    autoplayHoverPause: true,
+    responsive: {
+      767: {
+        items: 2,
+        slideBy: 2
+      }
+    }
+  });
+  /**********/
+
+  var dialog_steps = $("#dialog-steps").dialog({
+    autoOpen: false,
+    minHeight: 560,
+    minWidth: 350,
+    width: 560,
+    draggable: false,
+    modal: true,
+    close: closeModal,
+    position: {
+      my: 'center',
+      at: 'center'
+    }
+  });
+  var botonModal = $('.open-dialog');
+  $(botonModal).on("click", function () {
+    dialog_steps.dialog("open");
+    $(overlay).show('fade');
+  });
+  $('.close').on('click', function () {
+    dialog_steps.dialog('close');
+  });
+
+  function closeModal() {
+    $(overlay).hide('fade');
+    restartDialog(); // .show()
+  }
+
+  function restartDialog() {
+    var fieldsets = $('#msform > fieldset');
+    $(fieldsets).each(function (element, value) {
+      $(value).hide().css({
+        'transform': 'scale(1)',
+        'position': 'relative'
+      });
+    });
+    $(fieldsets[0]).css({
+      'opacity': '1',
+      'transform': 'scale(1)',
+      'position': 'relative',
+      'display': 'block'
+    });
+  }
+  /**********/
+
+
+  var current_fs, next_fs, previous_fs; //fieldsets
+
+  var left, opacity, scale; //fieldset properties which we will animate
+
+  var animating; //flag to prevent quick multi-click glitches
+
+  $(".next.btn").click(function () {
+    if (animating) return false;
+    animating = true;
+    current_fs = $(this).parent();
+    next_fs = $(this).parent().next(); //show the next fieldset
+
+    next_fs.show(); //hide the current fieldset with style
+
+    current_fs.animate({
+      opacity: 0
+    }, {
+      step: function step(now, mx) {
+        //as the opacity of current_fs reduces to 0 - stored in "now"
+        //1. scale current_fs down to 80%
+        scale = 1 - (1 - now) * 0.2; //2. bring next_fs from the right(50%)
+
+        left = now * 50 + "%"; //3. increase opacity of next_fs to 1 as it moves in
+
+        opacity = 1 - now;
+        current_fs.css({
+          'transform': 'scale(' + scale + ')',
+          'position': 'absolute'
+        });
+        next_fs.css({
+          'left': left,
+          'opacity': opacity
+        });
+      },
+      duration: 800,
+      complete: function complete() {
+        current_fs.hide();
+        animating = false;
+      },
+      //this comes from the custom easing plugin
+      easing: 'easeInOutBack'
+    });
+  });
+  $(".previous.btn").click(function () {
+    if (animating) return false;
+    animating = true;
+    current_fs = $(this).parent();
+    previous_fs = $(this).parent().prev(); //de-activate current step on progressbar
+
+    $("#progressbar li").eq($("fieldset").index(current_fs)).removeClass("active"); //show the previous fieldset
+
+    previous_fs.show(); //hide the current fieldset with style
+
+    current_fs.animate({
+      opacity: 0
+    }, {
+      step: function step(now, mx) {
+        //as the opacity of current_fs reduces to 0 - stored in "now"
+        //1. scale previous_fs from 80% to 100%
+        scale = 0.8 + (1 - now) * 0.2; //2. take current_fs to the right(50%) - from 0%
+
+        left = (1 - now) * 50 + "%"; //3. increase opacity of previous_fs to 1 as it moves in
+
+        opacity = 1 - now;
+        current_fs.css({
+          'left': left
+        });
+        previous_fs.css({
+          'transform': 'scale(' + scale + ')',
+          'opacity': opacity,
+          'position': 'relative'
+        });
+      },
+      duration: 800,
+      complete: function complete() {
+        current_fs.hide();
+        animating = false;
+      },
+      //this comes from the custom easing plugin
+      easing: 'easeInOutBack'
+    });
+  }); // $(".submit").click(function () {
+  //     restartDialog();
+  //     $('#dialog-form').dialog('close')
+  //     return false;
+  // })
+});
