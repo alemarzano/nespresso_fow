830ddb73a12a4ee8fbc3aa5d231dd2c873aff8b9
Ale Marzano <alejandromarzano@encendercomunicacion.com>
2020-08-21 13:20:14 -0300

    Imagenes de Speakers

--- js/app.js
+++ js/app.js
@@ -15,19 +15,19 @@ app.controller('oradoresCtrlr', function ($scope) {
   $scope.oradores = [{
     "id": "01",
     "nombre": "Alejandro Melamed",
-    "foto": "melamed.jpg",
+    "foto": "https://www.nespresso.com/ecom/medias/sys_master/public/13569936916510/melamed.jpg",
     "rol": "Ambassador",
     "descripcion": "Director de Humanize Consulting. Referente en temas del futuro del trabajo, liderazgo y cultura organizacional. Fue VP  de RRHH de Coca Cola, Gerente de RRHH de Molinos Rio de la Plata y consultor senior de Arthur Andersen, entre otras actividades."
   }, {
     "id": "02",
     "nombre": "Facundo Manes",
-    "foto": "manes.jpg",
+    "foto": "https://www.nespresso.com/ecom/medias/sys_master/public/13569937145886/manes.jpg",
     "rol": "Speaker",
     "descripcion": "Neurocientífico y neurólogo. Investigador del CONICET y presidente honorario de Fundación INECO. Profesor de neurociencias cognitivas, presidente electo de la ISFTD y consultor del MRC Cognition and Brain Sciences Unit de la Universidad de Cambridge."
   }, {
     "id": "03",
     "nombre": "Carla Quiroga",
-    "foto": "quiroga.jpg",
+    "foto": "https://www.nespresso.com/ecom/medias/sys_master/public/13569937866782/quiroga.jpg",
     "rol": "Moderadora",
     "descripcion": "Periodista con más de 25 años de experiencia  en los medios. Trabajó en canal 7, 26 TV, Telefe y P&E. También en la revista Negocios, el diario El Cronista y la revista Apertura. Actualmente es editora de los suplementos de propiedades e inmuebles comerciales del diario La Nación y columnista en la LN+."
   }];
